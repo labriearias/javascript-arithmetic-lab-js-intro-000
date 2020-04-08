cef21c42e7f1f0225c46258487716c21bf5ca311
Steven LaBrie <stevenlabrie@yahoo.com>
2020-04-08 18:27:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ console.log(add(900, 50));
 function subtract(a, b) {
   return a - b;
 }
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increment(n) {
+  return n++
+}
+
+function decrement(n) {
+  return n--
+}
+
+parseInt(n);
